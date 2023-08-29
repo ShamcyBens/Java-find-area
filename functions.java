96251f0b3b0f134fc9ead1672fab1ee57dfb7979
ShamcyBens <bernardshammah@gmail.com>
2023-08-29 12:58:17 +0300

    functions.java file updated

---
+++ functions.java
@@ -0,0 +1,12 @@
+public class functions{
+
+    public static void main(String[] args){
+        sayHi();
+
+    }
+
+    public static void sayHi(){
+        System.out.println("Hello");
+    }
+
+}
